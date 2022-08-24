7ccfaeb01855c4e1b368ecbab1ef98e311df3620
jjhwnn <jeahwan174@gmail.com>
2022-08-24 11:16:52 +0900

    add comment dartLang/basic_2.dart

--- dartLang/basic_02.dart
+++ dartLang/basic_02.dart
@@ -1,6 +1,4 @@
-
-main(){
-
+main() {
   List threeKingdoms = [];
   // 추가하기
   threeKingdoms.add("위");
@@ -16,24 +14,21 @@ main(){
   // 삭제하기
   threeKingdoms.removeAt(1);
 
-  
   threeKingdoms.remove('We');
-
 
   print(threeKingdoms.length);
 
   threeKingdoms.add(1);
 
-  
   print(threeKingdoms);
 
   List<String> threeKingdoms2 = [];
   //--------
   // Map : Key와 Value로 이루어진 Collection
   Map fruits = {
-    'apple':'사과',
-    'grape':'포도',
-    'watermelon':'사과',
+    'apple': '사과',
+    'grape': '포도',
+    'watermelon': '사과',
   };
   print(fruits['apple']);
   fruits['watermelon'] = '시원한 수박';
@@ -42,17 +37,17 @@ main(){
   print(fruits);
 
   Map<String, int> fruitsPrice = {
-    'apple' : 1000,
-    'grape' : 2000,
-    'watermelon' : 3000,
+    'apple': 1000,
+    'grape': 2000,
+    'watermelon': 3000,
   };
-
   // 옵션화 변수, null_safety
   // nullpointException 방지를 위해 ! 사용
   print(fruitsPrice['apple']);
-  int applePrice = fruitsPrice['apple']!; 
+  int applePrice = fruitsPrice['apple']!;
 
   int numA = 10;
   int? numB = 100;
+  // nullable
   numB = null;
 }
